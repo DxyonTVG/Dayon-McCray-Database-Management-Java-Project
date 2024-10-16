800e47c4f1bf439787f7fc7580f338b8c6102f53
TheViceGoat <ddm5544@psu.edu>
2024-10-15 20:33:24 -0400

    Final Project!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,44 @@
  * Course: IST 242
  * Author: Dayon McCray
  * Date Developed: 10/8/2024
- * Last Date Changed: 10/8/2024
- * Rev: Make Sure Code Work Add To UserMenu
+ * Last Date Changed: 10/15/2024
+ * Rev: Had to print out cust info first change!!!
 
  */
+
+import java.util.Scanner;
 
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Customer details!!!!
+        System.out.println("Enter Customer ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter Customer First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter Customer Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter Customer Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Enter Customer Email: ");
+        String email = scanner.nextLine();
+
+        // Create a Customer object
+        Customer customer = new Customer(id, firstName, lastName, phoneNumber, email);
+
+        // Pass the customer object to the UserMenu
+        System.out.println("Customer created: " + customer);
+
+
         UserMenu.main(args);
     }
 }
